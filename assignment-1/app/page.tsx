b667f9723ee5f0c091d695fd4c6a6848a29a6095
mahvish hasan <mahvishhasann@gmail.com>
2025-07-07 17:45:55 +0500

    feat: smart quote generator with JSON and ShadCN

---
+++ assignment-1/app/page.tsx
@@ -0,0 +1,10 @@
+import QuoteForm from "@/components/ui/QuoteForm"
+
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-center mb-6"> Quote Generator</h1>
+      <QuoteForm />
+    </main>
+  )
+}
